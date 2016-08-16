8038e87bf82bcf0aa0e66559dc39acc86875127c
dgo <dgo@ac8f9228-856d-4cd5-9d80-3d09b57bd24d>
2016-08-16 07:36:58 +0000

    new milestone

---
+++ java/jtg/jtg.basics.bundle/keytool.cmd
@@ -0,0 +1,1 @@
+"C:\Program Files\Java\jdk1.7.0_25\bin\keytool.exe" -genkey -dname "cn=Dimitrij Goldstein, ou=edgo, o=org, c=de" -alias edgo -keystore keychain.keys -storepass a1s2d3 -validity 36500
